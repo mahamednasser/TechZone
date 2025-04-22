a73e24fcb2e753eca625f87ad37a13044e8a26e5
Nasser <Nasser@DESKTOP-O6UT8EL>
2025-04-22 23:29:31 +0200

    Add project files.

---
+++ TechZoneWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TechZoneWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
